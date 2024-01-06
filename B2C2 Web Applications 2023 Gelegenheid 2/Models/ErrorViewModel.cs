4ea71a75787701a42cbe38884103c6744ec18044
Robert Zhou <1943669zhou@zuyd.nl>
2024-01-06 12:34:08 +0100

    Add project files.

---
+++ B2C2 Web Applications 2023 Gelegenheid 2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace B2C2_Web_Applications_2023_Gelegenheid_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
