5aebb4047f0c2d2f2c3f63c522c678ec2cc045bb
Robert Zhou <1943669zhou@zuyd.nl>
2024-01-09 10:56:49 +0100

    CRUD

--- B2C2 Web Applications 2023 Gelegenheid 2/Controllers/CollectionItemController.cs
+++ B2C2 Web Applications 2023 Gelegenheid 2/Controllers/CollectionItemController.cs
@@ -28,7 +28,73 @@ namespace B2C2_Web_Applications_2023_Gelegenheid_2.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(CollectionItem obj)
         {
-            _db.CollectionItems.Add(obj);
+            if (ModelState.IsValid)
+            {
+                _db.CollectionItems.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var itemFromDB = _db.CollectionItems.Find(id);
+
+            if (itemFromDB == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemFromDB);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(CollectionItem obj)
+        {
+            if (ModelState.IsValid) 
+            {
+                _db.CollectionItems.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var itemFromDB = _db.CollectionItems.Find(id);
+
+            if (itemFromDB == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemFromDB);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.CollectionItems.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.CollectionItems.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
