0b46892a9c9204b1f10424011e391e58eb68f1eb
Robert Zhou <1943669zhou@zuyd.nl>
2024-01-09 10:35:31 +0100

    CollectionItemController

---
+++ B2C2 Web Applications 2023 Gelegenheid 2/Controllers/CollectionItemController.cs
@@ -0,0 +1,36 @@
+﻿using B2C2_Web_Applications_2023_Gelegenheid_2.Data;
+using B2C2_Web_Applications_2023_Gelegenheid_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace B2C2_Web_Applications_2023_Gelegenheid_2.Controllers
+{
+    public class CollectionItemController : Controller
+    {
+        private readonly CollectionDBContext _db;
+
+        public CollectionItemController(CollectionDBContext db)
+        {
+            _db = db;        
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CollectionItem> objCollectionItemList = _db.CollectionItems;
+            return View(objCollectionItemList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CollectionItem obj)
+        {
+            _db.CollectionItems.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
